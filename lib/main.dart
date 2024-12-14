8d0ff2d5b2ad739be0ec75a4ee94ab8d694a6d9f
Mark Gil Calitisin <calitisinmarkgil20@gmail.com>
2024-12-14 19:51:33 +0800

    first project commit

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:firestore_basics/map_sample.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() {
+  //only include this if using a web
+  // Firebase.initializeApp(
+  //     options: FirebaseOptions(
+  //         apiKey: "AIzaSyBXZ9G9gMm9dULJR6ufHL0668xcjxEaZuA",
+  //         authDomain: "lakbayrizal-73493.firebaseapp.com",
+  //         projectId: "lakbayrizal-73493",
+  //         storageBucket: "lakbayrizal-73493.firebasestorage.app",
+  //         messagingSenderId: "731813576417",
+  //         appId: "1:731813576417:web:e42121fb63b119bbf301a0",
+  //         measurementId: "G-CCTWDT7L03"));
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MapWidget(),
+    );
+  }
+}
+
+class MyWidget extends StatefulWidget {
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          body: Center(
+        child: Text('This is an App'),
+      )),
+    );
+  }
+}
